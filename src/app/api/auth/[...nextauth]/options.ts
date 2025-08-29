8e56360e92bee9b2786937fd256d6ae1bdee13b4
JayantRautela <jayantrautela0205@gmail.com>
2025-08-30 01:37:19 +0530

    add next-auth for login

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,111 @@
+import { prisma } from "@/lib/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { NextAuthOptions } from "next-auth";
+import bcrypt from "bcryptjs";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            id: "credentials",
+            name: "credentails",
+            credentials: {
+                identifier: { label: "Email", type: "email"},
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials: any): Promise<any> {
+                console.log("Credentials received:", credentials);
+
+                try {
+
+                    const user = await prisma.user.findFirst({
+                        where: {
+                            email: credentials.identifier,
+                        },
+                        include: {
+                            student: true,
+                            alumni: true,
+                            admin: true
+                        }
+                    });
+
+                    console.log(user);
+                    if (!user) throw new Error("No user found with this email");
+
+                    const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password!);
+                    if (!isPasswordCorrect) throw new Error("Incorrect Password");
+
+                    return {
+                        id: user.id,
+                        email: user.email,
+                        role: user.role,
+                        student: user.student
+                            ? {
+                                id: user.student.id,
+                                name: user.student.name,
+                                course: user.student.course,
+                                branch: user.student.branch,
+                                isVerified: user.student.isVerified,
+                                currentYear: user.student.currentYear,
+                                profilePictureUrl: user.student.ProfilePictureUrl,
+                            }
+                            : undefined,
+                        alumni: user.alumni
+                            ? {
+                                id: user.alumni.id,
+                                name: user.alumni.name,
+                                phoneNumber: user.alumni.phoneNumber,
+                                currentCompany: user.alumni.currentCompany,
+                                yearOfExperience: user.alumni.yearOfExperience,
+                                passoutYear: user.alumni.passoutYear,
+                                isVerified: user.alumni.isVerified,
+                                branch: user.alumni.branch,
+                                course: user.alumni.course,
+                                profilePictureUrl: user.alumni.ProfilePictureUrl,
+                            }
+                            : undefined,
+                        admin: user.admin
+                            ? {
+                                id: user.admin.id,
+                                collegeName: user.admin.CollegeName,
+                                acceptedDomain: user.admin.acceptedDomain,
+                            }
+                            : undefined,
+                    };
+                } catch (error: any) {
+                    throw new Error(error);
+                }
+            },
+        })
+    ],
+    pages: {
+        signIn: '/sign-in'
+    },
+    session: {
+        strategy: "jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        async session({ session, token }) {
+            if (token) {
+                session.user.id = token.id;
+                session.user.email = token.email;
+                session.user.admin = token.admin;
+                session.user.alumni = token.alumni;
+                session.user.student = token.student;
+                session.user.role = token.role;
+            }
+            return session
+        },
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+                token.email = user.email;
+                token.admin = user.admin;
+                token.alumni = user.alumni;
+                token.student = user.student;
+                token.role = user.role;
+            }
+            return token;
+        }
+    }
+}
---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,87 @@
+import 'next-auth'
+import { DefaultSession } from 'next-auth';
+
+declare module 'next-auth' {
+    interface User {
+        id: string;
+        role: "ADMIN" | "STUDENT" | "ALUMNI"
+        email: string,
+
+        student?: {
+            id: string;
+            name: string;
+            course: string;
+            branch: string;
+            isVerified: boolean;
+            currentYear: string;
+            profilePictureUrl?: string;
+        };
+
+        alumni?: {
+            id: string;
+            name: string;
+            phoneNumber: string;
+            currentCompany: string;
+            yearOfExperience: number;
+            passoutYear: number;
+            isVerified: boolean;
+            branch: string;
+            course: string;
+            profilePictureUrl?: string;
+        },
+
+        admin?: {
+            id: string;
+            collegeName: string;
+            acceptedDomain: string;
+        };
+    }
+    interface Session {
+        user: {
+            id: string;
+            role: 'STUDENT' | 'ALUMNI' | 'ADMIN';
+            email: string;
+
+            student?: User['student'];
+            alumni?: User['alumni'];
+            admin?: User['admin'];
+        } & DefaultSession['user'];
+    }
+}
+
+declare module 'next-auth/jwt' {
+    interface JWT {
+        id: string;
+        role: 'STUDENT' | 'ALUMNI' | 'ADMIN';
+        email: string;
+
+        student?: {
+            id: string;
+            name: string;
+            course: string;
+            branch: string;
+            isVerified: boolean;
+            currentYear: string;
+            profilePictureUrl?: string;
+        };
+
+        alumni?: {
+            id: string;
+            name: string;
+            phoneNumber: string;
+            currentCompany: string;
+            yearOfExperience: number;
+            passoutYear: number;
+            isVerified: boolean;
+            branch: string;
+            course: string;
+            profilePictureUrl?: string;
+        };
+
+        admin?: {
+            id: string;
+            collegeName: string;
+            acceptedDomain: string;
+        };
+    }
+}
