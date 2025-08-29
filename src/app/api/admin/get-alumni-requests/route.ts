211698c30709e471c69f9e86b23870d2756011fb
JayantRautela <jayantrautela0205@gmail.com>
2025-08-29 20:22:03 +0530

    add GET endpoint for fetching unverified alumni requests

---
+++ src/app/api/admin/get-alumni-requests/route.ts
@@ -0,0 +1,55 @@
+import { prisma } from "@/lib/prisma";
+
+export async function GET(request: Request) {
+    try {
+        const alumniRequest = await prisma.alumni.findMany({
+            where: {
+                isVerified: false
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                phoneNumber: true,
+                cgpa: true,
+                currentCompany: true,
+                yearOfExperience: true,
+                passoutYear: true,
+                branch: true,
+                course: true,
+                isOpenToTakeMentorshipSession: true,
+                isVerified: true,
+                ProfilePictureUrl: true,
+                createdAt: true
+            }
+        });
+
+        if (alumniRequest.length === 0) {
+            return Response.json({
+                success: false,
+                message: "No verification request found"
+            }, 
+            {
+                status: 404
+            });
+        }
+
+        return Response.json({
+            success: true,
+            message: "Alumni verification request recieved",
+            alumniRequest
+        }, 
+        {
+            status: 200
+        });
+    } catch (error) {
+        console.error(`Error in fetching alumni request :- ${error}`);
+        return Response.json({
+            success: false,
+            message: "Error in fetching alumni request"
+        }, 
+        {
+            status: 500
+        });
+    }
+}
