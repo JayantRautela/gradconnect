959fa239214cba30e1cad1b1ad6cb065a205834c
JayantRautela <jayantrautela0205@gmail.com>
2025-08-31 01:00:50 +0530

    feat: create admin dashboard layout and update branding in login page

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,43 @@
+'use client'
+import { Button } from "@/components/ui/button";
+import { Users } from "lucide-react";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { data: session } = useSession();
+  const admin = session?.user.admin;
+  return (
+    <main>
+        <nav className="p-4 md:p-2 shadow-md bg-gray-50">
+          <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
+            <div className="flex gap-2 items-center">
+              <div className="w-16 h-16 bg-blue-500 rounded-2xl flex items-center justify-center">
+                <Users className="w-8 h-8 text-white" />
+              </div>
+              <span className="text-2xl font-semibold text-blue-500">GradConnect</span>
+            </div>
+            {
+              session ? (
+                <>
+                  <span className='mr-4'>Welcome, {admin?.collegeName}</span>
+                  <Button className='w-full md:w-auto cursor-pointer' onClick={() => signOut()}>Logout</Button>
+                </>
+              ) : (
+                <Link href='/sign-in'>
+                  <Button className='w-full md:w-auto cursor-pointer'>
+                    Login
+                  </Button>
+                </Link>
+              )
+            }
+          </div>
+        </nav>
+        {children}
+    </main>
+  );
+}
--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -107,7 +107,7 @@ export default function Login () {
           
           <h1 className="text-5xl md:text-6xl font-bold mb-6">
             Welcome to{" "}
-            <span className="text-blue-500">AlumniConnect</span>
+            <span className="text-blue-500">GradConnect</span>
           </h1>
           
           <p className="text-xl text-gray-600 max-w-3xl mx-auto leading-relaxed">
