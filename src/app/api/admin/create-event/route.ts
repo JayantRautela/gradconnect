690c2ba81cfe88533f5b4bf2c416d8f4e33e3454
JayantRautela <jayantrautela0205@gmail.com>
2025-09-05 19:45:27 +0530

    add route to create an event

---
+++ src/app/api/admin/create-event/route.ts
@@ -0,0 +1,81 @@
+import { prisma } from "@/lib/prisma";
+import { Mode } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+    api_key: process.env.CLOUDINARY_API_KEY!,
+    api_secret: process.env.CLOUDINARY_API_SECRET!,
+});
+
+export default async function POST (request: Request) {
+    try {
+        const session = await getServerSession();
+        const admin = session?.user.admin;
+
+        if (!admin) {
+            return Response.json({
+                success: false,
+                message: "Unauthorized"
+            },
+            {
+                status: 403
+            });
+        }
+
+        const formData = await request.formData();
+
+        const title = formData.get("title") as string;
+        const place = formData.get("place") as string;
+        const time = new Date(formData.get("time") as string);
+        const description = formData.get("description") as string;
+        const mode = formData.get("mode") as Mode;
+        const file = formData.get("eventBanner") as File | null
+
+        let eventBannerUrl: string = "";
+        if (file) {
+            const buffer = Buffer.from(await file.arrayBuffer());
+            const uploadResult = await new Promise<{ secure_url: string }>((resolve, reject) => {
+                cloudinary.uploader.upload_stream({ folder: "event-banner" }, (err, result) => {
+                    if (err || !result) reject(err);
+                    else resolve(result as { secure_url: string });
+                }).end(buffer);
+            });
+            eventBannerUrl = uploadResult.secure_url;
+        }
+
+        const event = await prisma.event.create({
+            data: {
+                title: title,
+                description: description,
+                time: time,
+                place: place,
+                mode: mode,
+                eventBannerUrl: eventBannerUrl,
+                adminId: admin.id
+            },
+            include: {
+                createdBy: true
+            }
+        });
+
+        return Response.json({
+            success: true,
+            message: "Event created successfully",
+            event: event
+        },
+        {
+            status: 201
+        });
+    } catch (error) {
+        console.error(`Error in creating event :- ${error}`);
+        return Response.json({
+            success: false,
+            message: "Error in creating event"
+        }, 
+        {
+            status: 500
+        });
+    }
+}
