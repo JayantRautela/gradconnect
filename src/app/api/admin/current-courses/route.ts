b94a171bf010e8458f0e90ed70e2c9de5d94318a
JayantRautela <jayantrautela0205@gmail.com>
2025-09-11 22:19:17 +0530

    add admin route to get students

---
+++ src/app/api/admin/current-courses/route.ts
@@ -0,0 +1,42 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function GET (request: NextRequest) {
+    try {
+        const courses = await prisma.student.findMany({
+            distinct: ["course"],
+            select: { 
+                course: true 
+            } 
+        });
+
+        if (courses.length === 0) {
+            return NextResponse.json({
+                success: false,
+                message: "No alumni found"
+            }, 
+            {
+                status: 404
+            }); 
+        }
+
+        return NextResponse.json({ 
+            success: true,
+            message: "passout year fetched",
+            years: courses.map((y) => y.course) 
+        }, 
+        {
+            status: 200
+        });
+    } catch (error) {
+        console.error(`Error in fetching course :- ${error}`);
+        return NextResponse.json({
+            success: false,
+            message: "Error in fetching course"
+        }, 
+        {
+            status: 500
+        });
+    }
+}
---
+++ src/app/api/admin/get-all-students/route.ts
@@ -0,0 +1,90 @@
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import { NextRequest, NextResponse } from "next/server";
+import { Course } from "@prisma/client";
+
+export async function GET (request: NextRequest) {
+    try {
+        const session = await getServerSession(authOptions);
+
+        if (!session?.user?.email) {
+            return NextResponse.json({
+                success: false,
+                message: "Unauthorized"
+            }, 
+            {
+                status: 401
+            });
+        }
+
+        const admin = await prisma.user.findUnique({
+            where: {
+                email: session.user.email
+            }, 
+            include: {
+                admin: true
+            }
+        });
+
+        if (!admin?.admin) {
+            return NextResponse.json(
+            { 
+                success: false, 
+                message: "Admin not found" 
+            },
+            { 
+                status: 404 
+            });
+        }
+
+        // example request :- fetch(`/api/alumni?course=2022`);
+        // this when admin click on one of the card in the dashboard.
+        const { searchParams } = new URL(request.url);
+        const course = searchParams.get("course") as Course; 
+
+        if (!course) {
+            return NextResponse.json({
+                success: false,
+                message: "Course is required"
+            },
+            {
+                status: 400
+            });
+        }
+
+        const student = await prisma.student.findMany({
+            where: {
+                course: course,
+                collegeName: admin.admin.CollegeName,
+                isVerified: true
+            },
+            include: {
+                user: {
+                    select: {
+                        email: true,
+                        role: true
+                    }
+                }
+            }
+        });
+
+        return NextResponse.json({
+            success: true,
+            message: "Student fetched successfully",
+            student: student
+        },
+        {
+            status: 200
+        });
+    } catch (error) {
+        console.error(`Error in fetching alumnis :- ${error}`);
+        return NextResponse.json({
+            success: false,
+            message: "Error in fetching alumnis"
+        },
+        {
+            status: 500
+        });
+    }
+}
